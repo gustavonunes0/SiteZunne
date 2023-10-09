6ffda30a397ddc7f91b5a16abe20ae46c3414f2b
Gustavo Nunes <108197715+gustavonunes0@users.noreply.github.com>
2023-10-09 13:40:14 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,10 @@ const Home = () => {
     //     setIsPlaying(false);
     //     }
     // };
+
+    const adicionarHifens = (texto) => {
+        return texto.replace(/(\w+)/g, '$1\u200B'); 
+    };
     
     return (
         <>
@@ -54,9 +58,9 @@ const Home = () => {
                                 </a>
                             </S.BotaoInvestir>
                     </div>
-                    <S.SpanPrimeiroTexto>O Zunne é um programa de investimento de impacto, que prioriza negócios de impacto positivo
+                    <S.SpanPrimeiroTexto>{adicionarHifens(O Zunne é um programa de investimento de impacto, que prioriza negócios de impacto positivo
                                                  liderados por mulheres, pessoas negras e/ou indígenas, com o intuito de erradicar a pobreza e
-                                                  reduzir desigualdades no Norte e Nordeste do Brasil, democratizando o investimento de impacto.</S.SpanPrimeiroTexto>
+                                                  reduzir desigualdades no Norte e Nordeste do Brasil, democratizando o investimento de impacto.)}</S.SpanPrimeiroTexto>
                     <S.ContainerDuplo>
                         <S.ImagemAbelha src='/abelha.svg'/>
                         <S.SpanTextoAbelha>Nossa inspiração vem da conexão com a natureza: as
