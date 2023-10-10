d54f530d0f7c222a131e009426a2851b1465ffba
Gustavo Nunes <108197715+gustavonunes0@users.noreply.github.com>
2023-10-10 14:14:54 -0300

    Update home.ts

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -827,7 +827,7 @@ export const Ponto = styled.div<PontoProps>`
   left: calc(${props => props.left || '0'} + 3.5%);
 
   @media screen and (max-width: 1050px) {
-    left: calc(${props => props.left || '0'} + 4%);
+    left: calc(${props => props.left || '0'} + 3%);
     top: 1.3rem;
   }
   @media screen and (max-width: 580px) {
@@ -860,7 +860,7 @@ export const Ponto2 = styled.div<PontoProps>`
     top: 1.3rem;
   }
   @media screen and (max-width: 1050px) {
-    left: calc(${props => props.left || '0'} + 3.5%);
+    left: calc(${props => props.left || '0'} + 3%);
     margin-left: 0rem;
     top: 1.3rem;
   }
